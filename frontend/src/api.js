c1a2da172ff90420fde6fe36fd17f8f0c3349386
abhishekdev45 <95223863+abhishekdev45@users.noreply.github.com>
2024-05-09 09:39:42 +0530

    Add files via upload

---
+++ frontend/src/api.js
@@ -0,0 +1,6 @@
+// export const url = 'http://localhost:5000'
+export const url = "http://193.203.162.161:4006";
+// export const url = 'https://backend.globalmailer.webdesys.com'
+// export const url = 'https://backend.test.webdesys.com'
+
+// sales id pass -> "ASf-b[]IaT~8"
